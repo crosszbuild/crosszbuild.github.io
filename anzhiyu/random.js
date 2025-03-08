21747e5fd00062a69cea7400211b3d16ea31e115
crosszbuild <940745119@qq.com>
2025-03-08 13:48:04 +0800

    Site updated: 2025-03-08 13:48:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/21/hello-world/","2025/03/05/插曲/","2025/02/18/关于本站（以后ver）/","2025/02/25/音乐球一则/","2025/09/01/奇点/","2025/03/10/关于本站/"];function toRandomPost(){
+var posts=["2025/02/21/hello-world/","2025/02/18/关于本站（以后ver）/","2024/09/01/奇点/","2025/01/01/关于本站/","2025/03/05/插曲/","2025/02/20/音乐球一则/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
